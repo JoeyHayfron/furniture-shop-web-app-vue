a9c0c5393e0fbe7a6c1eadc741144c81f334e0d7
Joseph Hayfron Acquah <joeacq81@gmail.com>
2024-06-11 00:38:08 +0000

    Setup fonts

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header></header>
+  <header>HIM</header>
 
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  font-family: 'Butler';
+}
+</style>
